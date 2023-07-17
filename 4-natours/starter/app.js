2b584080293cc39ba8956a608c3d84065599b2ec
Souransu Sen <souransu2sen@gmail.com>
2023-07-17 13:50:57 +0530

    authentication, authorization and security complete

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,20 +13,59 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
+// Set security http headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limiting the number of requests from an IP
+const limiter = rateLimit({
+  max: 3,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in one hour!',
+});
+app.use('/api', limiter);
+
+// Body parser; reading data from body into req.body
+app.use(
+  express.json({
+    limit: '10kb',
+  }),
+);
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS(cross-site scripting)
+app.use(xss());
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+// Preventing parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingAverage',
+      'ratingQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+
+// Test middleware
 app.use((req, res, next) => {
   req.requestedAt = new Date().toISOString;
   // console.log(req.headers);
   next();
 });
-
-app.use(express.json());
-
-app.use(express.static(`${__dirname}/public`));
 
 // ROUTES
 
